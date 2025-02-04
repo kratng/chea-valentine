380bcad45b7e5e155234c52e4c0061e91df50686
kratng <chhayvoreakrotn@gmail.com>
2025-02-04 22:22:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 $(document).ready(function () {
   let currentPage = 0;
   const totalPages = 3;
+  let musicPlayed = false;
 
   $('.container').click(function () {
-    if (currentPage < totalPages) {
-      if (currentPage > 0) {
-        $(`.page${currentPage}`).hide();
-      }
-      currentPage++;
+    if (currentPage > 0) {
+      $(`.page${currentPage}`).hide();
+    }
+
+    currentPage++;
+
+    if (currentPage <= totalPages) {
       $(`.page${currentPage}`).fadeIn();
     } else {
-      $(`.page${currentPage}`).hide();
       currentPage = 0;
     }
 
-    // Play music on first click
-    if (currentPage === 1) {
-     
- document.getElementById('valentineAudio').play();
+    // Play music only once
+    if (!musicPlayed) {
+      document.getElementById('valentineAudio').play();
+      musicPlayed = true;
     }
   });
 
   $('.container').mouseenter(function () {
-    $('.card').stop().animate({
-      top: '-90px'
-    }, 'slow');
+    $('.card').stop().animate({ top: '-90px' }, 'slow');
   }).mouseleave(function () {
-    $('.card').stop().animate({
-      top: '5px'
-    }, 'slow');
+    $('.card').stop().animate({ top: '5px' }, 'slow');
   });
 });
