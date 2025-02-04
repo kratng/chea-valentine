1e6a1b95345df3dfe70b68bff3cb8fd11d545fdf
kratng <chhayvoreakrotn@gmail.com>
2025-02-04 21:12:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 <!-- script.js -->
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
+pages[currentPage].style.display = "flex";
+
+document.getElementById("backgroundMusic").play();
 
 function nextPage() {
     if (currentPage < pages.length - 1) {
         pages[currentPage].style.display = "none";
         currentPage++;
-        pages[currentPage].style.display = "block";
+        pages[currentPage].style.display = "flex";
     }
 }
-
-document.getElementById("backgroundMusic").play();
