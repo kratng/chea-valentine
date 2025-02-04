0ad229247abf42a44002c3f7f6f3f03339262e32
kratng <chhayvoreakrotn@gmail.com>
2025-02-04 21:04:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,13 @@
-let slideIndex = 0;
+<!-- script.js -->
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
 
-// Function to start the slideshow
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-
-    // Hide all slides initially
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function nextPage() {
+    if (currentPage < pages.length - 1) {
+        pages[currentPage].style.display = "none";
+        currentPage++;
+        pages[currentPage].style.display = "block";
     }
-
-    // Increment slideIndex
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1; // Restart slideshow after last slide
-    }
-
-    // Show the current slide
-    slides[slideIndex - 1].style.display = "block";
-
-    // Change slide every 3 seconds
-    setTimeout(showSlides, 3000);
 }
 
-// Start slideshow when heart is clicked
-document.getElementById("heart").addEventListener("click", function() {
-    // Hide the welcome screen
-    document.querySelector(".welcome-screen").style.display = "none";
-
-    // Show the slideshow
-    let slideshow = document.querySelector(".slideshow");
-    slideshow.style.display = "block";
-    
-    // Start slideshow
-    showSlides();
-    
-    // Start playing the music
-    document.getElementById("backgroundMusic").play();
-    
-    // Show final message after slideshow ends
-    setTimeout(function() {
-        slideshow.style.display = "none"; // Hide slideshow
-        document.getElementById("finalMessage").style.display = "block"; // Show final message
-    }, 9000); // Total slideshow duration (3 seconds * number of slides)
-});
+document.getElementById("backgroundMusic").play();
